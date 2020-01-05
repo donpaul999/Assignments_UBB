0a1a05459eae4313409ccd0cef2d8af8b5cac89f
Paul Colța <43109542+donpaul999@users.noreply.github.com>
2020-01-05 09:24:27 +0200

    poate e corect

---
+++ Assignment 11/structure.py
@@ -0,0 +1,34 @@
+class Structure:
+    def __init__(self):
+       self._data = []
+
+    def __iter__(self):
+        return StructureIterator(self)
+
+    def __next__(self):
+        pass
+
+    def add(self, object):
+        self._data.append(object)
+
+
+class StructureIterator:
+    def __init__(self, structure):
+        self._structure = structure
+        self._index = 0
+
+    def __next__(self):
+        if self._index < (len(self._structure._data)):
+            result = self._structure._data[self._index]
+            self._index += 1
+            return result
+        raise  StopIteration
+
+
+s = Structure()
+s.add("mar")
+s.add("par")
+for i in s:
+    print(i)
+
+
