ce37d3db755a03cd1e63a9ea5681654b88b8678e
Paul Colța <43109542+donpaul999@users.noreply.github.com>
2020-05-04 07:38:03 +0300

    update

---
+++ Semester 2/OOP/Exam_4_May/cmake-build-debug/CMakeFiles/Exam_4_May.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exam_4_May.dir/main.cpp.o"
+  "Exam_4_May"
+  "Exam_4_May.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exam_4_May.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
