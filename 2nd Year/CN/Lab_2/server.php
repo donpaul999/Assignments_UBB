89da762454bbb58f67135c92200780264eb1c577
Paul Colța <43109542+donpaul999@users.noreply.github.com>
2020-10-12 17:11:11 +0300

    Part of DB Lab2

--- 2nd Year/CN/Lab_2/server.php
+++ 2nd Year/CN/Lab_2/server.php
@@ -1,10 +1,7 @@
 <?php
-    $s=socket_create(AF_INET,SOCK_DGRAM,0);                  //create a UDP socket
-    socket_bind($s,'0.0.0.0',5555);                          //bind on all interfaces on port 5555
-    socket_recvfrom($s,$message,1024,0,$client_ip,$client_port);     //read max 10 bytes from socket $s into buffer $b
-    //echo "Received: ".$b." from IP=".$client_ip.":".$client_port;
-    $arr = json_decode($message, true);
-    $max = $arr['1'] > $arr['2'] ? $arr['1'] : $arr['2'];
-
-    socket_sendto($s,$max, 3,0,$client_ip,$client_port);   //send a message back to the client
+    $s=socket_create(AF_INET,SOCK_STREAM,0);
+    socket_bind($s,"0.0.0.0",9999);
+    socket_listen($s);
+    $cs=socket_accept($s);
+    socket_send($cs,"Hello",5,0);
 ?>
---
+++ 2nd Year/Databases/Lab2/Paul_Stefan_Colta_ms.sql
@@ -0,0 +1,136 @@
+--INSERT
+
+SET IDENTITY_INSERT ubb_schema.Categories ON
+
+INSERT INTO ubb_schema.Categories(id, name, description) VALUES (2, 'tech', 'Tech Resources')
+
+SET IDENTITY_INSERT ubb_schema.Categories OFF
+
+
+SET IDENTITY_INSERT ubb_schema.Clients ON
+
+INSERT INTO ubb_schema.Clients(id, name, email, phone) VALUES ('1', 'Telekom', 'client@telekom.ro', '0712312312')
+
+SET IDENTITY_INSERT ubb_schema.Clients OFF
+
+
+SET IDENTITY_INSERT ubb_schema.Projects ON
+
+--wrong client_id
+INSERT INTO ubb_schema.Projects(id, name, description, client_id, category_id) VALUES ('1', 'First Project', 'This is the first project', '12', '1')
+--[2020-10-12 16:05:54] [23000][547] The INSERT statement conflicted with the FOREIGN KEY constraint "client_id". The conflict occurred in database "ubb_db", table "ubb_schema.Clients", column 'id'.
+
+
+--correct client_id
+INSERT INTO ubb_schema.Projects(id, name, description, client_id, category_id) VALUES ('1', 'First Project', 'This is the first project', '1', '1')
+
+SET IDENTITY_INSERT ubb_schema.Projects OFF
+
+
+SET IDENTITY_INSERT ubb_schema.Programmers ON
+
+INSERT INTO ubb_schema.Programmers(id, name, email, phone, birthday) VALUES ('1', 'Paul', 'paul@infonow.ro', '0799265027', '20010726')
+
+SET IDENTITY_INSERT ubb_schema.Programmers OFF
+
+
+SET IDENTITY_INSERT ubb_schema.ProjectsProgrammers ON
+
+--wrong programmer_id
+INSERT INTO ubb_schema.ProjectsProgrammers(id, project_id, programmer_id) VALUES ('1', '1', '2')
+
+--correct programmer_id
+INSERT INTO ubb_schema.ProjectsProgrammers(id, project_id, programmer_id) VALUES ('1', '1', '1')
+
+
+
+
+
+--UPDATE
+
+UPDATE ubb_schema.Projects
+SET description='Telekom Projects' WHERE client_id IN(1,3,5,7)
+
+UPDATE ubb_schema.Categories
+SET description='This is one of the first 3 categories of our projects' WHERE id BETWEEN 1 AND 3
+
+UPDATE ubb_schema.Programmers
+SET name='Stefan' WHERE name LIKE 'Paul%'
+
+
+
+
+--DELETE
+
+--I insert a new category
+SET IDENTITY_INSERT ubb_schema.Categories ON
+
+INSERT INTO ubb_schema.Categories(id, name) VALUES (3, 'cars')
+
+SET IDENTITY_INSERT ubb_schema.Categories OFF
+
+DELETE ubb_schema.Categories
+WHERE description IS NULL
+
+
+--I inserted 2 Holiday dates
+SET IDENTITY_INSERT ubb_schema.Holidays ON
+
+INSERT INTO ubb_schema.Holidays(id, programmer_id, start_date, end_date) VALUES (1, 1, '20050720', '20050725')
+INSERT INTO ubb_schema.Holidays(id, programmer_id, start_date, end_date) VALUES (2, 1, '20250720', '20250725')
+SET IDENTITY_INSERT ubb_schema.Holidays OFF
+
+DELETE FROM ubb_schema.Holidays
+WHERE start_date <= GETDATE()
+
+
+
+
+
+
+--UNION
+
+SELECT id FROM ubb_schema.Projects
+WHERE name LIKE '%project%' OR name LIKE 'First%'
+UNION
+SELECT id FROM ubb_schema.Clients
+
+
+SELECT id, birthday FROM ubb_schema.Programmers
+WHERE name LIKE '%Paul%' OR name LIKE 'Stefan%'
+UNION
+SELECT id, start_date FROM ubb_schema.Holidays
+
+
+
+--INTERSEC
+
+INSERT INTO ubb_schema.Programmers(name, email, phone, birthday) VALUES ('Alex', 'a@infonow.ro', '0712312312', '20000523')
+
+SELECT id, name FROM ubb_schema.Clients
+WHERE id IN (2,4,6,8)
+INTERSECT
+SELECT id, name FROM ubb_schema.Programmers
+
+SELECT name FROM ubb_schema.Technologies
+INTERSECT
+SELECT name FROM ubb_schema.Projects
+WHERE id IN (1, 4)
+
+
+
+--EXCEPT
+
+
+SELECT id, name FROM ubb_schema.Projects
+EXCEPT
+SELECT id, name FROM ubb_schema.Technologies
+WHERE id NOT IN (1,3,5)
+
+SELECT id, name FROM ubb_schema.Clients
+EXCEPT
+SELECT id, name FROM ubb_schema.Programmers
+WHERE id NOT IN (2,4,6,8)
+
+
+
