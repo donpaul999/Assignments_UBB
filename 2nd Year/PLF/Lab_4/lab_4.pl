de2dc8a5698b6f72ae574d6c4ace9ad47b23f8f9
Paul Colța <43109542+donpaul999@users.noreply.github.com>
2020-10-30 13:36:31 +0200

    Lab_3

---
+++ 2nd Year/PLF/Lab_4/lab_4.pl
@@ -0,0 +1,3 @@
+% 11. “Colouring” a map. n countries are given; write a predicate to determine all possibilities of colouring n
+% countries with m colours, such that two adjacent countries not having the same colour
+
