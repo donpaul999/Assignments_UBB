1a43fe953c3565c8d78d96dd5c6831b4dfb94e4d
Paul Colța <43109542+donpaul999@users.noreply.github.com>
2020-11-06 00:11:26 +0200

    update plf lab

--- 2nd Year/PLF/Lab_4/.idea/lab_4_f.pl
+++ 2nd Year/PLF/Lab_4/.idea/lab_4_f.pl
@@ -1,10 +1,47 @@
-check([], _).
-check([(H,CH),(S,CS)|T], Edges):-
-    member((H,S), Edges),
-    CH \== CS,
-    check([(_,CS)|T], Edges).
+edge(a, b).
+edge(b, c).
+edge(c, d).
 
-allcombinations(_, _, _, N, N, _).
-allcombinations([H|T], Edges, [CH|CT], N, CI, Ans):-
-    NI is CI + 1,
-    allcombinations(T, Edges, [CH|CT], N, NI, [Ans|(H,CH)])
+color(red).
+color(green).
+color(blue).
+color(orange).
+
+city(a).
+city(b).
+city(c).
+city(d).
+
+%is_inside(L:List, E:Element)
+% (i,i) deterministic
+% 0, if l is empty
+% 1, if l1 == E
+% is_inside(E, l2..ln) otherwise
+
+is_inside(H, [H|_]):- !.
+is_inside(E, [_|T]):-
+    	is_inside(E, T).
+
+
+colorMap(Ans) :-
+  findall((X, Y), edge(X, Y), Edges),
+  findall(X, city(X), Cities),
+  findall(hasColor(X, _), member(X, Cities), Ans),
+  createConstraint(Edges,Ans),
+  colorNodes(Ans).
+
+%createConstraint(List:Edges,List:Ans)
+%createConstraint(i, o)
+
+createConstraint([],_).
+createConstraint([(V1,V2)|T],Ans):-
+  is_inside(hasColor(V1,C1),Ans),
+  is_inside(hasColor(V2,C2),Ans),
+  dif(C1,C2),
+  createConstraint(T,Ans).
+
+%
+colorNodes([]).
+colorNodes([hasColor(_,C)|Nodes]) :-
+  color(C),
+  colorNodes(Nodes).
