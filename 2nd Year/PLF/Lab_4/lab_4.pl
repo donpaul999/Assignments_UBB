e6c41523fbb2861bddb09a8070323749928c5fc2
Paul Colța <43109542+donpaul999@users.noreply.github.com>
2020-11-09 11:11:00 +0200

    update

--- 2nd Year/PLF/Lab_4/lab_4.pl
+++ 2nd Year/PLF/Lab_4/lab_4.pl
@@ -1,36 +1,35 @@
-% 11. “Colouring” a map. n countries are given; write a predicate to determine all possibilities of colouring n
-% countries with m colours, such that two adjacent countries not having the same colour
-%SOL 1
+% 11. Colouring a map. n countries are given; write a
+% predicate to determine all possibilities of colouring n
+% countries with m colours, such that two adjacent countries not
+% having the same colour.
+% countries:
 
-color(red).
-color(blue).
-color(green).
+%is_inside(E:Elem, L:List)
+% (i,i) deterministic
+% 0, if l is empty
+% 1, if l1 == E
+% is_inside(E, l2..ln) otherwise
+is_inside(H, [H|_]):- !.
+is_inside(E, [_|T]):-
+    is_inside(E, T).
 
-country(romania).
-country(france).
-country(russia).
-
-
-edge(romania, france).
-edge(france, russia).
-
-
-colorMap(Ans) :-
-  findall((X, Y), edge(X, Y), Edges),
-  findall(X, country(X), Countries),
+colorMap(Countries, Edges, Colors, Ans) :-
   findall(hasColor(X, _), member(X, Countries), Ans),
   createConstraint(Edges,Ans),
-  colorCountries(Ans).
+  colorCountries(Colors, Ans).
 
+%createConstraint(E:List)
+% 1, if L is empty
+% 0, if C1 == C2, where Ci is Vi's color, where V is part of an edge
+% createConstraint(E2...En)
 createConstraint([],_).
 createConstraint([(V1,V2)|T],Ans):-
-  member(hasColor(V1,C1),Ans),
-  member(hasColor(V2,C2),Ans),
+  is_inside(hasColor(V1,C1),Ans),
+  is_inside(hasColor(V2,C2),Ans),
   dif(C1,C2),
   createConstraint(T,Ans).
 
-
-colorCountries([]).
-colorCountries([hasColor(_,C)|Nodes]) :-
-  color(C),
-  colorCountries(Nodes).
+colorCountries(_, []).
+colorCountries(Colors, [hasColor(_,C)|CC]) :-
+  member(C, Colors),
+  colorCountries(Colors, CC).
