4db4e7125b73ee29196b0f0b90c7b431b7f0a0ea
Paul Colța <43109542+donpaul999@users.noreply.github.com>
2021-01-26 21:10:30 +0200

    update

--- 2nd Year/APM/Lucru_examen/src/Main_stream.java
+++ 2nd Year/APM/Lucru_examen/src/Main_stream.java
@@ -2,14 +2,25 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/*
+? - read null, write Object
+extend - read, get
+super - write, add
+? extend A
+? super C
+
+ */
 public class Main_stream {
     public static void main(String[] args){
+        //collect(Collectors.toList())
+        //collect(Collectors.joining(""))
+
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8,9,10,11,12,14,15);
-        String ans = numbers
+        Integer ans = numbers
                     .stream()
-                    .filter(s -> s % 3 == 0 && s % 2 == 0)
-                    .map(s -> "At" + s.toString() + "km")
-                    .collect(Collectors.joining());
+                    .filter(s -> s % 3 == 0 || s % 7 == 0)
+                    .map(s -> s - 1)
+                    .reduce(0, (total, s) -> (total + s) % 5);
         System.out.println(ans);
     }
 }
