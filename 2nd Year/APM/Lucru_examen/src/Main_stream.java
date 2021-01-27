d94f794bc1e4c51e510de23c68f1c993465555fb
Paul Colța <43109542+donpaul999@users.noreply.github.com>
2021-01-27 13:59:42 +0200

    update

--- 2nd Year/APM/Lucru_examen/src/Main_stream.java
+++ 2nd Year/APM/Lucru_examen/src/Main_stream.java
@@ -14,13 +14,21 @@ public class Main_stream {
     public static void main(String[] args){
         //collect(Collectors.toList())
         //collect(Collectors.joining(""))
-
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8,9,10,11,12,14,15);
+        /*
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8,9,10,11,12,14,15, 16);
         Integer ans = numbers
                     .stream()
                     .filter(s -> s % 3 == 0 || s % 7 == 0)
                     .map(s -> s - 1)
                     .reduce(0, (total, s) -> (total + s) % 5);
+
+         */
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8,9,10,11,12,14,15, 16);
+        List<Integer> ans = List.of(numbers
+                .stream()
+                .filter(s -> s % 4 == 0)
+                .map(s -> s + 1)
+                .reduce(0, (total, s) -> (total + s) % 2));
         System.out.println(ans);
     }
 }
