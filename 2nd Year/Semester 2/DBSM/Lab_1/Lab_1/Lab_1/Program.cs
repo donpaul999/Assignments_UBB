32531b0c856f59168c4fe52783295f87fbe918c1
Paul Colța <coltapaulstefan@gmail.com>
2021-03-04 12:56:07 +0200

    update

--- 2nd Year/Semester 2/DBSM/Lab_1/Lab_1/Lab_1/Program.cs
+++ 2nd Year/Semester 2/DBSM/Lab_1/Lab_1/Lab_1/Program.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data;
+using System.Data.SqlClient;
 namespace Lab_1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source = PAULCOLTA060E\\SQLEXPRESS; Initial Catalog = it_company; " +
+                " Integrated Security = SSPI";
         }
     }
 }
