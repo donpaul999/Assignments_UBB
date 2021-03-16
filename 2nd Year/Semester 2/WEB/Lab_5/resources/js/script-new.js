5767e770d191460b1532c6c028c1f5fbe71258c2
Paul Colța <43109542+donpaul999@users.noreply.github.com>
2021-03-16 09:34:01 +0200

    update HW

--- 2nd Year/Semester 2/WEB/Lab_5/resources/js/script-new.js
+++ 2nd Year/Semester 2/WEB/Lab_5/resources/js/script-new.js
@@ -1,19 +1,33 @@
 $(document).ready(function() {
-    isDragging = false;
-    $("div")
-        .mousedown(function () {
-            isDragging = false;
+    removed = '';
+    removed_id = '';
+    $(".draggable-handler")
+        .on('dragstart', function (){
+            $("#text").text("Drag start!" + this.id);
+            removed = $(this).get();
+            removed_id = $(this).attr('id');
+            $(this).css('display', 'none');
+           // console.log(1);
         })
-        .mousemove(function () {
-            isDragging = true;
+        .on('dragend', function (){
+            console.log(removed);
+            $("#text").text("Drag end!");
+            $("#" + removed_id).detach();
+            $(this).stop(true,true).animate({top:'20px'},200);
+            //TODO - FIX THIS
+            $(removed).insertAfter(this);
+            // console.log(1);
         })
-        .mouseup(function () {
-            var wasDragging = isDragging;
-            isDragging = false;
-            if (!wasDragging) {
-                console.log("false");
-            } else {
-                console.log("true");
-            }
-        });
+        .on('dragover', function (){
+            $("#text").text("Drag over!" + this.id);
+            $(this).stop(true,true).animate({top:'-20px'},200);
+
+            // console.log(1);
+        })
+        .on('drop', function (){
+            $("#text").text("Drop!");
+            // console.log(1);
+        })
 });
+
+//drag start, drag over
