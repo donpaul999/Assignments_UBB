24b29c1b83ae74deabe02505528db75db5e0d826
Paul Colța <43109542+donpaul999@users.noreply.github.com>
2021-03-16 16:48:49 +0200

    UPDATE PW HW

--- 2nd Year/Semester 2/WEB/Lab_5/resources/js/script-new.js
+++ 2nd Year/Semester 2/WEB/Lab_5/resources/js/script-new.js
@@ -6,21 +6,25 @@ $(document).ready(function() {
             $("#text").text("Drag start!" + this.id);
             removed = $(this).get();
             removed_id = $(this).attr('id');
-            $(this).css('display', 'none');
+            //$(this).css('opacity', '0.01');
+            $(this).fadeTo(200, 0);
            // console.log(1);
         })
         .on('dragend', function (){
-            console.log(removed);
             $("#text").text("Drag end!");
-            $("#" + removed_id).detach();
-            $(this).stop(true,true).animate({top:'20px'},200);
-            //TODO - FIX THIS
-            $(removed).insertAfter(this);
-            // console.log(1);
+            //console.log(removed);
+            $('#' + removed_id).css('opacity', '1');
+            removed = '';
+            removed_id = '';
         })
         .on('dragover', function (){
+            //console.log(removed_id);
             $("#text").text("Drag over!" + this.id);
-            $(this).stop(true,true).animate({top:'-20px'},200);
+            if(removed_id < this.id)
+                $('#' + removed_id).before($(this));
+            else
+                $(this).before($('#' + removed_id));
+            //$(this).stop(true,true).animate({top:'-20px'},200);
 
             // console.log(1);
         })
