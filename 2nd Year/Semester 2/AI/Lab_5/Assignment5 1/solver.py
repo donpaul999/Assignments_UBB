f83a15084af9c444e12018819cb9c4529c7c0e8b
Paul Colța <43109542+donpaul999@users.noreply.github.com>
2021-04-16 15:28:13 +0300

    update

--- 2nd Year/Semester 2/AI/Lab_5/Assignment5 1/solver.py
+++ 2nd Year/Semester 2/AI/Lab_5/Assignment5 1/solver.py
@@ -6,7 +6,8 @@ In this file your task is to write the solver function!
 
 
 def solver(t,w):
-    
+    t = 7
+    w = -0.5
     """
     Parameters
     ----------
@@ -27,7 +28,7 @@ def solver(t,w):
     teta = {"NVB" : [-100, -25, -40], "NB": [-40, -10, -25], "N":[-20, 0, -10], "ZO":[-5, 5, 0], "P":[0, 20, 10], "PB":[10, 40, 25], "PVB":[25, 100, 40]}
     omega = {"NB" :[-20, -3, -8], "N" :[-6, 0, -3] , "ZO" : [-1, 1, 0], "P" :[0, 6, 3], "PB" : [3, 20, 8]}
     force = {'NVB+NB': 'NVVB', 'NVB+N': 'NVVB', 'NVB+ZO': 'NVB', 'NVB+P': 'NB', 'NVB+PB': 'N', 'NB+NB': 'NVVB', 'NB+N': 'NVB', 'NB+ZO': 'NB', 'NB+P': 'N', 'NB+PB': 'Z', 'N+NB': 'NVB', 'N+N': 'NB', 'N+ZO': 'N', 'N+P': 'Z', 'N+PB': 'P', 'ZO+NB': 'NB', 'ZO+N': 'N', 'ZO+ZO': 'Z', 'ZO+P': 'P', 'ZO+PB': 'PB', 'P+NB': 'N', 'P+N': 'Z', 'P+ZO': 'P', 'P+P': 'PB', 'P+PB': 'PVB', 'PB+NB': 'Z', 'PB+N': 'P', 'PB+ZO': 'PB', 'PB+P': 'PVB', 'PB+PB': 'PVVB', 'PVB+NB': 'P', 'PVB+N': 'PB', 'PVB+ZO': 'PVB', 'PVB+P': 'PVVB', 'PVB+PB': 'PVVB'}
-
+    products = {'NVVB': -32, 'NVB': -24, 'NB': -16, 'N': -8, 'Z': 0, 'P': 8, 'PB': 16, 'PVB': 24, 'PVVB': 32}
 
 
     niuteta = {}
@@ -71,11 +72,9 @@ def solver(t,w):
 
     sum = 0
     product = 0
-    x = -32
     for key in forces:
         sum += forces[key]
-        product += forces[key] * x
-        x += 8
+        product += forces[key] * products[key]
     try:
         f = sum / product
     except:
