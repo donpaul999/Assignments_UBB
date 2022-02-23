ea96760308edcee2e9afa568eb335806f8d22093
Ralb R <coltapaulstefan@gmail.com>
2022-02-23 20:03:38 +0200

    Update myFirstFunction.m

--- 3rd Year/Semester 2/CN/Lab_1/myFirstFunction.m
+++ 3rd Year/Semester 2/CN/Lab_1/myFirstFunction.m
@@ -1,7 +1,7 @@
-function  g = myFirstFunction  (n)
+function   g = myFirstFunction  (n)
   if (n  == 0)
-    g  =  1
+    g  =  2
   else
-    g =  1 + 1 /(1 +  (myFirstFunction  (n-1))
+    g =  1 + 1 /(1 +  (myFirstFunction  (n-1)))
   endif
 endfunction
